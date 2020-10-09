7e10ff8925bf10a40560ab1d4a08c8570fa6c515
z3t4z00k <zetazook@gmail.com>
2020-10-09 23:40:05 +0530

    Added countdown

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -8,7 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+
+    let eggTimers = ["Soft":300, "Medium":420, "Hard":720];
+    var secondsRemaining = 60;
+    @IBOutlet weak var label: UILabel!
+    var timer = Timer()
     
-
-
+    @IBAction func hardness(_ sender: UIButton) {
+        
+        timer.invalidate()
+        
+        let hardness = sender.currentTitle!;
+        
+        secondsRemaining = eggTimers[hardness]!;
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func updateTimer(){
+        if(secondsRemaining>0){
+            secondsRemaining-=1
+        }
+        else if(secondsRemaining == 0){
+            timer.invalidate()
+            label.text = "Done!"
+        }
+    }
+    
 }
